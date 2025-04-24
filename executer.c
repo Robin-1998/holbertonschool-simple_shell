953b608fbd372bccd69c1b5339e1dc01fbb12720
Robin-1998 <10616@holbertonstudents.com>
2025-04-24 17:19:33 +0200

    ajout de tout les commentaires

--- executer.c
+++ executer.c
@@ -1,37 +1,36 @@
 #include "main.h"
 /**
- * _fork_execve_wait - fonction qui exécute un programme
+ * _fork_execve_wait - fonction qui exécute une commande en créant un
+ * processus fils
  * @argument : correspond à notre argument dans notre terminal
  * @environ : Tableau de chaînes représentant les variables d’environnement
  */
 void _fork_execve_wait(char **argument, char **environ)
 {
-	pid_t pid = fork();
+	pid_t pid = fork(); /*on créé au processus enfant*/
 	int status;
 
-	if (pid == -1)
+	if (pid == -1) /*on gère le cas d'erreur*/
 	{
 		perror("Echec de création du nouveau processus");
 		exit(1);
 	}
-	if (pid == 0)
-	{
+	if (pid == 0)/*execve remplace le process actuel par la commande demandée*/
+	{ /*on gère aussi le cas d'erreur si ça ne fonctionne pas et on quitte*/
 		if (execve(argument[0], argument, environ) == -1)
 		{
 			perror("Erreur dans l'exécution");
 			exit(1);
 		}
 	}
-	else
+	else /*on bloque le processus parent tant que l'enfant n'a pas terminé*/
 	{
-		wait(&status);
+		wait(&status); /* on met en pause tant que ce n'est pas fini*/
 	}
 }
 
 /**
- * _getpath - Recherche dans les variables d’environnement la commande à
- * exécuter, et remplace `argument[0]` par le chemin absolu si elle est
- * trouvée dans l’un des répertoires du PATH.
+ * _getpath - Cherche où se trouve la commande dans les dossiers du PATH
  * @argument : Tableau de chaînes représentant la commande et ses arguments
  * @environ : Tableau de chaînes représentant les variables d’environnement
  * Return: int
@@ -40,7 +39,7 @@ int _getpath(char **argument, char **environ)
 {
 	int i = 0;
 	char *token, *PATH = NULL, contenant[1024], *copie;
-
+/* On cherche la ligne qui commence par "PATH=" dans les variables d'env*/
 	while (environ[i])
 	{
 		if (_strcmp(environ[i], "PATH=", 5) == 0)
@@ -50,30 +49,30 @@ int _getpath(char **argument, char **environ)
 		}
 		i++;
 	}
-	if (PATH == NULL)
+	if (PATH == NULL)/*Si on trouve pas PATH, on arrête tout*/
 		return (1);
-	copie = _strdup(PATH);
-
+	copie = _strdup(PATH);/*On duplique la variable PATH pour pouvoir*/
+/*la découper sans modifier l'originale*/
 	if (copie == NULL)
 	{
-		perror("strdup");
+		perror("strdup"); /*si la duplication échoue*/
 		return (1);
-	}
+	} /*On découpe chaque dossier du PATH (séparés par des ":")*/
 	token = strtok(copie, ":");
 	while (token)
-	{
+	{/*on stock dans contenant notre chemin complet*/
 		sprintf(contenant, "%s/%s", token, *argument);
-		if (access(contenant, X_OK) == 0)
-		{
+		if (access(contenant, X_OK) == 0) /*access c'est exécutable*/
+		{/*Si commande trouvé, on remplace argument[0] par le chemin absolu*/
 			free(*argument);
 			*argument = _strdup(contenant);
 			if (!*argument)
 			{
-				perror("strdup");
+				perror("strdup");/*Si on n'arrive pas à sauvegarder le chemin*/
 				free(copie);
 				return (1);
 			}
-		}
+		} /*Sinon on teste le dossier suivant dans PATH*/
 			token = strtok(NULL, ":");
 	}
 	free(copie);
